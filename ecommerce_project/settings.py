7c0941f1fe2f882a1e0bb968ba8561c99aa6984e
Zuber Shekh <zuber.shekh7@gmail.com>
2021-06-28 17:00:12 +0530

    Added core app and basic templates

--- ecommerce_project/settings.py
+++ ecommerce_project/settings.py
@@ -38,6 +38,9 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+
+    # local
+    'core.apps.CoreConfig',
 ]
 
 MIDDLEWARE = [
@@ -55,7 +58,9 @@ ROOT_URLCONF = 'ecommerce_project.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [
+            os.path.join(BASE_DIR, 'templates'),
+        ],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [

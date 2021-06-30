fffbbe93844a085d01519baf2e52de739b0b07d3
Zuber Shekh <zuber.shekh7@gmail.com>
2021-06-30 19:57:42 +0530

    feat: add products page

--- ecommerce_project/urls.py
+++ ecommerce_project/urls.py
@@ -9,6 +9,7 @@ from django.conf.urls.static import static
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('core.urls')),
+    path('products/', include('products.urls'))
 ]
 
 if settings.DEBUG:
--- products/views.py
+++ products/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
 
-# Create your views here.
+from .models import Product
+
+def list_products(request):
+    products = Product.objects.all()
+    context = {
+        'products': products,
+    }
+    return render(request, 'products/list.html', context)
